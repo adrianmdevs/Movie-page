4ba9f5239165e76149b11ad4f309da7eebff5616
adrianmdevs <murigucharles3@gmail.com>
2024-05-06 13:22:59 +0300

    first commit

---
+++ READMe.md
@@ -0,0 +1,7 @@
+**DOM can ;**
+>>Add or remove HTML nodes.
+>> Add,remove or Modify CSS styles.
+>> Active user interactions by attaching event listeners like click and   submit
+
+**This Project demonstrates the use of the Javascript DOM**
+ >> 
---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded'), function(){
+    const list = document.querySelector('#movie-list ul');
+    const forms = document.forms;
+
+    //delete movie
+    list.addEventListener('click', (e)=>{
+        if(e.target.className == 'delete'){
+            const li = e.target.par
+        }
+    })
+
+
+
+}
